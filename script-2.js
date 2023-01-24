da951fca501c8c91db8a0d22f0dba06939d94e76
Tommaso Nannicini <tnannicini@gmail.com>
2023-01-24 20:38:32 +0100

    Agg esercitazioni con classi di oggetti

--- script-2.js
+++ script-2.js
@@ -2,27 +2,42 @@ let block = document.getElementById('block')
 
 
 
-class Person {
-    // qua definisco la classe
-    constructor(name, surname, age) {
-      this.name = name
-      this.surname = surname
-      this.age = age
+
+  class Person {
+    constructor(name,surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
-  }
+
+    compareAge(other) {
+        if (this.age < other.age){
+             block.innerText = `${other.name} è più vecchio di ${this.name}.`
+        }else if (this.age > other.age){
+             block.innerText = `${other.name} è più giovane di ${this.name}.`
+        }else{
+             block.innerText = `${other.name} è della stessa età di ${this.name}.`
+        }
+    }
+}
+  
 
 
-  let p1 = new Person('Mario', 'Rossi', 12)
-  let p2 = new Person('Luigi', 'Verdi', 42)
-  let p3 = new Person('Jacqueline', 'Chan', 65)
+
+let p1 = new Person('Mario', 'Rossi', 12)
+let p2 = new Person('Luigi', 'Verdi', 42)
+let p3 = new Person('Jacqueline', 'Chan', 65)
 
 
+console.log(p1.compareAge(p2));
+console.log(p2.compareAge(p1));
+console.log(p1.compareAge(p3));
 
-  if (p1.age > p2.age && p1.age > p3.age) {
-    block.innerText = `Ciao sono ${p1.name} ${p1.surname} e sono la persona più vecchia `
-  } else if (p2.age > p1.age && p2.age > p3.age) { 
-    block.innerText = `Ciao sono ${p2.name} ${p2.surname} e sono la persona più vecchia `
-  } else if (p3.age > p2.age && p3.age > p1.age) { 
-    block.innerText = `Ciao sono ${p3.name} ${p3.surname} e sono la persona più vecchia `
-    
-  } 
+//     if (p1.age > p2.age && p1.age > p3.age) {
+//       block.innerText = `Ciao sono ${p1.name} ${p1.surname} e sono la persona più vecchia `
+//     } else if (p2.age > p1.age && p2.age > p3.age) { 
+//       block.innerText = `Ciao sono ${p2.name} ${p2.surname} e sono la persona più vecchia `
+//     } else if (p3.age > p2.age && p3.age > p1.age) { 
+//       block.innerText = `Ciao sono ${p3.name} ${p3.surname} e sono la persona più vecchia `
+      
+//     } }
